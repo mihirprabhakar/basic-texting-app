638c338b192cbc9246e199c6af63ceaa453705cd
mihirprabhakar <mihirprabhakar6@gmail.com>
2026-06-23 14:00:28 +0530

    added new chat routes with working

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const Chat=require("./models/chat.js");
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine","ejs");
 app.use(express.static(path.join(__dirname,"public")));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 main()
 .then(()=>{console.log("connection successful")
@@ -26,8 +28,25 @@ app.get("/",(req,res)=>{
 //index route
 app.get("/chats",async (req,res)=>{
   let chats=await Chat.find();
-  console.log(chats);
+  // console.log(chats);
   res.render("index.ejs",{chats});
+});
+
+
+app.get("/chats/new",(req,res)=>{
+  res.render("new.ejs");
+});
+
+app.post("/chats",(req,res)=>{
+  let{from,to,msg}=req.body;
+  let newChat=new Chat({
+    from:from,
+    to:to,
+    msg:msg,
+    created_at:new Date()
+  });
+  newChat.save().then(res=>{console.log("chat saved");}).catch(err=>{console.log(err);});
+  res.redirect("/chats");
 });
 
 app.listen(8080,()=>{
