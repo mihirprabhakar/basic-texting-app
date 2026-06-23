b5ef02a06f9f57809227c13f895c3e27cef46035
mihirprabhakar <mihirprabhakar6@gmail.com>
2026-06-23 15:28:57 +0530

    added the update and delete feture

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const app=express();
 const mongoose=require("mongoose");
 const path=require("path");
 const Chat=require("./models/chat.js");
+const methodOverride=require("method-override");
+
 //middlewares
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine","ejs");
 app.use(express.static(path.join(__dirname,"public")));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
 
 main()
 .then(()=>{console.log("connection successful")
@@ -37,6 +40,13 @@ app.get("/chats/new",(req,res)=>{
   res.render("new.ejs");
 });
 
+
+app.get("/chats/:id/edit",async (req,res)=>{
+  let {id}=req.params;
+  let chat=await Chat.findById(id);
+  res.render("edit.ejs",{chat});
+});
+
 app.post("/chats",(req,res)=>{
   let{from,to,msg}=req.body;
   let newChat=new Chat({
@@ -46,6 +56,21 @@ app.post("/chats",(req,res)=>{
     created_at:new Date()
   });
   newChat.save().then(res=>{console.log("chat saved");}).catch(err=>{console.log(err);});
+  res.redirect("/chats");
+});
+
+app.put("/chats/:id",async (req,res)=>{
+  let {id}=req.params;
+  let{msg:newMsg}=req.body;
+  let updatedChat=await Chat.findByIdAndUpdate(id,{msg:newMsg},{runValidators:true,new:true});
+  console.log(updatedChat);
+  res.redirect("/chats");
+});
+
+app.delete("/chats/:id",async (req,res)=>{
+  let {id}=req.params;
+  let chatToBeDeleted=await Chat.findByIdAndDelete(id);
+  console.log(chatToBeDeleted);
   res.redirect("/chats");
 });
 
